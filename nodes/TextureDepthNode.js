d86abef20489a48b93cc6c4aac7eba7f3a188e90
Ossama Jouini <skymen75019@gmail.com>
2025-11-17 05:00:54 +0100

    removed unnecessary swizzle

--- nodes/TextureDepthNode.js
+++ nodes/TextureDepthNode.js
@@ -9,17 +9,17 @@ export const TextureDepthNode = new NodeType(
     webgl1: {
       dependency: "",
       execution: (inputs, outputs) =>
-        `    float ${outputs[0]} = texture2D(samplerDepth, ${inputs[0]}.xy).r;`,
+        `    float ${outputs[0]} = texture2D(samplerDepth, ${inputs[0]}).r;`,
     },
     webgl2: {
       dependency: "",
       execution: (inputs, outputs) =>
-        `    float ${outputs[0]} = texture(samplerDepth, ${inputs[0]}.xy).r;`,
+        `    float ${outputs[0]} = texture(samplerDepth, ${inputs[0]}).r;`,
     },
     webgpu: {
       dependency: ``,
       execution: (inputs, outputs) =>
-        `    var ${outputs[0]}: f32 = textureSample(textureDepth, samplerDepth, ${inputs[0]}.xy);`,
+        `    var ${outputs[0]}: f32 = textureSample(textureDepth, samplerDepth, ${inputs[0]});`,
     },
   },
   "Texture",
--- nodes/TextureFrontNode.js
+++ nodes/TextureFrontNode.js
@@ -13,21 +13,21 @@ export const TextureFrontNode = new NodeType(
     webgl1: {
       dependency: "",
       execution: (inputs, outputs) =>
-        `    vec4 ${outputs[0]} = texture2D(samplerFront, ${inputs[0]}.xy);\n` +
+        `    vec4 ${outputs[0]} = texture2D(samplerFront, ${inputs[0]});\n` +
         `    vec3 ${outputs[1]} = ${outputs[0]}.xyz;\n` +
         `    float ${outputs[2]} = ${outputs[0]}.a;`,
     },
     webgl2: {
       dependency: "",
       execution: (inputs, outputs) =>
-        `    vec4 ${outputs[0]} = texture(samplerFront, ${inputs[0]}.xy);\n` +
+        `    vec4 ${outputs[0]} = texture(samplerFront, ${inputs[0]});\n` +
         `    vec3 ${outputs[1]} = ${outputs[0]}.xyz;\n` +
         `    float ${outputs[2]} = ${outputs[0]}.a;`,
     },
     webgpu: {
       dependency: ``,
       execution: (inputs, outputs) =>
-        `    var ${outputs[0]}: vec4<f32> = textureSample(textureFront, samplerFront, ${inputs[0]}.xy);\n` +
+        `    var ${outputs[0]}: vec4<f32> = textureSample(textureFront, samplerFront, ${inputs[0]});\n` +
         `    var ${outputs[1]}: vec3<f32> = ${outputs[0]}.xyz;\n` +
         `    var ${outputs[2]}: f32 = ${outputs[0]}.a;`,
     },
